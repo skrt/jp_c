c4b7d2db0481ff45a7967b012f469811e2490c60
skrt_c <JP11356@G010D31310193M.local>
2016-06-16 10:22:47 +0900

    演算子、制御構文について

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,5 +2,41 @@
 
 import UIKit
 
-var str = "Hello, playground"
-print(str)
+var x = 300
+
+
+switch x {
+    case 0 :
+        "zero"
+    case 1...9:
+        "1桁"
+    case 10...99:
+        "2桁"
+    case 100...999:
+        "3桁"
+    default:
+        "無いよ"
+}
+
+for _ in 1...10{
+    x += 2
+}
+
+
+for var i = 0;i < 10;i++ {
+    x += 2
+}
+
+
+var n:Int = 0
+
+while 10 > n++ {
+    "index:" + String(n)
+}
+
+let friend = ["さとう","さいとう","こばやし"]
+var m:Int = 0
+repeat {
+    print(friend[m])
+    m++
+} while(m < friend.count)
