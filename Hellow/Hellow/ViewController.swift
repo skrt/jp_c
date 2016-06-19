839c0bc52fbe301db0284dbdb4f6e022680c92a6
skrt <sakai.ryota.049@gmail.com>
2016-06-20 00:39:02 +0900

    UIButton勉強
    UIレイアウト初歩勉強

--- Hellow/Hellow/ViewController.swift
+++ Hellow/Hellow/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBAction func sayHello() {
+        label.text = "こんにちは"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,4 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
